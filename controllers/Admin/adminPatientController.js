0dfdc2f1facae23d67fc04456cf671d08aaab471
Shreyasrana18 <srana1_be20@thapar.edu>
2023-07-12 01:46:28 +0530

    patientlist func from admin view

--- controllers/Admin/adminPatientController.js
+++ controllers/Admin/adminPatientController.js
@@ -5,7 +5,7 @@ const Patient = require("../../models/patientModel");
 const mongoose = require("mongoose");
 
 const patientList = asyncHandler(async (req, res) => {
-    const admininfo = await Admin.find({ userID: new mongoose.Types.ObjectId(req.params.userID) }).populate('patientInformation');
+    const admininfo = await Admin.find({ userID: new mongoose.Types.ObjectId(req.params.userID) });
     // check if the user is authorized to access the personal information
     if (admininfo[0].userID.valueOf().toString() != req.user.id) {
         res.status(401);
@@ -15,6 +15,18 @@ const patientList = asyncHandler(async (req, res) => {
         res.status(404);
         throw new Error("Admin not found");
     }
+    if (admininfo > 0) {
+        console.log('Data is accessible from the Admin schema.');
+        // You can access the data here
+    } else {
+        console.log('No data found from the Admin schema.');
+    }
+    const ids = admininfo[0].patientInformation;
+    const patientData = [];
+    ids.forEach(async (id) => {
+        patient = await Patient.find({ userID: new mongoose.Types.ObjectId(id) });
+        console.log(patient[0].patientPersonalInformation);
+    });
     res.status(201).json(admininfo[0].patientInformation);
 });
 
@@ -30,6 +42,7 @@ const addPatient = asyncHandler(async (req, res) => {
         res.status(404);
         throw new Error('Patient not found');
     }
+    console.log(patient[0]);
     hospital[0].patientInformation.push(patient[0].userID);
     await hospital[0].save();
     res.status(201).json({ message: 'Patient added to hospital successfully' });
@@ -39,7 +52,6 @@ const removePatientDb = asyncHandler(async (req, res) => {
     try {
         const { patientID } = req.body;
         const hospital = await Admin.find({ userID: new mongoose.Types.ObjectId(req.params.userID) });
-
         const index = hospital[0].patientInformation.indexOf(patientID);
         if (index !== -1) {
             hospital[0].patientInformation.pull(patientID);
